50e5200f98cac3ae5d15737ea8e9ec3330aebe25
Rahul Shukla <rahulshukla422@gmail.com>
2019-09-06 12:56:53 +0530

    Poc application for the adding the new guest user from c# application

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientId = "your client id ";
+            string clientSecret = "your client secret";
+            string tenantID = "your azure ad tanant/ directory id";
+
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                                                                            .Create(clientId)
+                                                                            .WithTenantId(tenantID)
+                                                                            .WithClientSecret(clientSecret)
+                                                                            .Build();
+
+            ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var invitation = new Invitation
+            {
+                InvitedUserEmailAddress = "rahulshukla422@gmail.com",
+                InviteRedirectUrl = "https://yourapplication.com"
+            };
+
+            graphClient.Invitations
+               .Request()
+               .AddAsync(invitation).GetAwaiter().GetResult();
+
+            Console.ReadLine();
+        }
+    }
+}
